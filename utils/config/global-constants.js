60fd9ff67e988397cda0da59c8f21e731ae1b6c9
bigdigital <bigdigital.kov@gmail.com>
2022-12-15 11:50:05 +0200

    update wf id

--- utils/config/global-constants.js
+++ utils/config/global-constants.js
@@ -1,7 +1,7 @@
 export const WF_INFO = "watchdrip_info";
 export const WF_INFO_LAST_UPDATE = "watchdrip_info_last"; 
 export const WATCHDRIP_APP_ID = "28962";
-export const WATCHDRIP_WF_ID = "20001";
+export const WATCHDRIP_WF_ID = "28963";
 
 export const WATCHDRIP_CONFIG = "watchdrip_config";
 export const WATCHDRIP_CONFIG_LAST_UPDATE = "watchdrip_info_last_upd";
